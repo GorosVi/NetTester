cd023f86300e5262b1577ac258ec075e80ea3155
GorosVi <gorosvia@gmail.com>
2017-08-14 15:19:40 +0300

    Switched to RGMII constraints from Ethond BSP

--- FPGA_Project/TOP.sv
+++ FPGA_Project/TOP.sv
@@ -2,27 +2,28 @@ module TOP(
 	input  wire clk_25m_i,
 	input  wire srst_i,
 
-	input  wire clk_125m_rgmii_rx_i,
-	input  wire [3:0] rgmii_rx_i,
-	input  wire       rgmii_rx_ctl_i,
+	input  wire trg_rx_clk_i  [0],
+	input  wire trg_rxd_i     [0][3:0],
+	input  wire trg_rx_ctrl_i [0],
+	output wire trg_tx_clk_o  [0],
+	output wire trg_txd_o     [0][0:3],
+	output wire trg_tx_ctrl_o [0],
 
-	output wire clk_125m_rgmii_tx_o,
-	output wire [3:0] rgmii_tx_o,
-	output wire       rgmii_tx_ctl_o,
+	output wire trg_mdc_o     [0],
+	inout  wire trg_mdio_io   [0],
 
-	inout wire phy_mdio_mdio,
-	output wire phy_mdio_mdc
+	output wire trg_nreset_o  [0],
 );
 
 wire srst_n = ~srst_i;
 
 wire clk_125m_out;
 MAC_PLL mac_pll_inst(
-		.refclk (clk_25m_i),   //  refclk.clk
-		.rst (srst_i),      //   reset.reset
-		.outclk_0(clk_125m_out)  // outclk0.clk
-	); 
-	
+		.refclk (clk_25m_i),    // refclk.clk
+		.rst (srst_i),          // reset.reset
+		.outclk_0(clk_125m_out) // outclk0.clk
+	);
+
 wire clk_125m_system, clk_125m_mac_tx;
 assign clk_125m_system = clk_125m_out;
 assign clk_125m_mac_tx = clk_125m_out;
@@ -31,14 +32,14 @@ assign clk_125m_rgmii_tx_o = clk_125m_mac_tx;
 DelayTesterSystem u0 (
 	.qsys_system_clk_clk                 (clk_125m_system),                 //                 qsys_system_clk.clk
 	.qsys_system_reset_reset_n           (srst_n),
-	
-	.mac_pcs_mac_rx_clock_connection_clk (clk_125m_rgmii_rx_i), // mac_pcs_mac_rx_clock_connection.clk
-	.mac_mac_rgmii_connection_rgmii_in   (rgmii_rx_i),   //        mac_mac_rgmii_connection.rgmii_in
-	.mac_mac_rgmii_connection_rx_control (rgmii_rx_ctl_i), //                                .rx_control
 
-	.mac_pcs_mac_tx_clock_connection_clk (clk_125m_mac_tx),  // mac_pcs_mac_tx_clock_connection.clk
-	.mac_mac_rgmii_connection_rgmii_out  (rgmii_tx_o),  //                                .rgmii_out
-	.mac_mac_rgmii_connection_tx_control (rgmii_tx_ctl_o), //                                .tx_control
+	.mac_pcs_mac_rx_clock_connection_clk (trg_rx_clk_i[0]), // mac_pcs_mac_rx_clock_connection.clk
+	.mac_mac_rgmii_connection_rgmii_in   (trg_rxd_i[0]),   //        mac_mac_rgmii_connection.rgmii_in
+	.mac_mac_rgmii_connection_rx_control (trg_rx_ctrl_i[0] ), //                                .rx_control
+
+	.mac_pcs_mac_tx_clock_connection_clk (trg_tx_clk_o[0]),  // mac_pcs_mac_tx_clock_connection.clk
+	.mac_mac_rgmii_connection_rgmii_out  (trg_txd_o[0]),  //                                .rgmii_out
+	.mac_mac_rgmii_connection_tx_control (trg_tx_ctrl_o[0]), //                                .tx_control
 
 	.mac_mac_mdio_connection_mdc         (mac_mdio_clk),    //   mac_mac_mdio_connection.mdc
 	.mac_mac_mdio_connection_mdio_in     (mac_mdio_in),    //  .mdio_in
@@ -47,8 +48,8 @@ DelayTesterSystem u0 (
 );
 
 logic mac_mdio_in, mac_mdio_out, mac_mdio_oen, mac_mdio_clk;
-assign phy_mdio_mdc = mac_mdio_clk;
-assign phy_mdio_mdio = ( mac_mdio_oen ) ? ( mac_mdio_out ) : ( 1'bz ); // Check OEN default value!!!
+assign trg_mdc_o = mac_mdio_clk;
+assign trg_mdio_io = ( mac_mdio_oen ) ? ( mac_mdio_out ) : ( 1'bz ); // Check OEN default value!!!
 assign mac_mdio_in = phy_mdio_mdio;
 
 
